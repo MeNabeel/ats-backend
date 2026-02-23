75315bc16a046fe25bfc7bed3be2980817f97427
MeNabeel <nabeelijaz559@gmail.com>
2026-02-23 17:27:56 +0500

    Added new features and fixed responsive design

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(limiter);
 
 // CORS
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173' || 'https://ats-frontend-1.onrender.com',
     credentials: true
 }));
 
